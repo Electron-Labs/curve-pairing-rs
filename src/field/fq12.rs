02f26000280d3eeffe5d6058ee1d0e908672b317
utsavjnn <utsavjnn@users.noreply.github.com>
2023-04-10 13:09:38 +0530

    test: fq12 basic tests
    Signed-off-by: utsavjnn <utsavjnn@users.noreply.github.com>

--- src/field/fq12.rs
+++ src/field/fq12.rs
@@ -2,6 +2,7 @@ use std::ops;
 use crate::field::fq12;
 use crate::field::fq2::Fq2;
 use crate::field::fq6::Fq6;
+use crate::field::fq::Fq;
 
 #[derive(Debug, Clone)]
 pub struct Fq12 {
@@ -21,7 +22,7 @@ impl Fq12 {
 
     pub fn mul_by_residue(&self, val: Fq6) -> Fq6 {
         return Fq6::fq6(
-            [val.val[2].clone()*self.non_residue, val.val[0].clone(), val.val[1].clone()],
+            [val.val[2].clone()*self.non_residue.clone(), val.val[0].clone(), val.val[1].clone()],
             val.non_residue,
             val.forbenius_coeffs_c1,
             val.forbenius_coeffs_c2
@@ -42,7 +43,7 @@ impl Fq12 {
 
     pub fn forbenius_map(&self, power: usize) -> Fq12 {
         return Fq12::fq12(
-            [self.val[0].clone().forbenius_map(power), self.val[1].clone().forbenius_map(power).mult_by_fq2(self.frobenius_coeffs_c1[power % 12])],
+            [self.val[0].clone().forbenius_map(power), self.val[1].clone().forbenius_map(power).mult_by_fq2(self.forbenius_coeffs_c1[power % 12].clone())],
             self.non_residue.clone(),
             self.forbenius_coeffs_c1.clone()
         )
@@ -200,6 +201,182 @@ impl ops::Div<Fq12> for Fq12 {
     type Output = Fq12;
     fn div(self, rhs: Fq12) -> Self::Output {
         return self*rhs.inverse();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+    use num_bigint::BigInt;
+    use super::*;
+
+    fn get_fq2(val1: BigInt, val2: BigInt) -> Fq2 {
+        let fq_field_modulus = BigInt::from_str("21888242871839275222246405745257275088696311157297823662689037894645226208583").unwrap();
+        let fq2_non_residue = Fq::fq(fq_field_modulus.clone(), BigInt::from_str("21888242871839275222246405745257275088696311157297823662689037894645226208582").unwrap());
+        let fq2_forbenius_coeffs_c1 = [Fq::fq(fq_field_modulus.clone(), BigInt::from(1)),Fq::fq(fq_field_modulus.clone(), BigInt::from_str("21888242871839275222246405745257275088696311157297823662689037894645226208582").unwrap())];
+        return Fq2::fq2(
+            [
+                Fq::fq(fq_field_modulus.clone(), BigInt::from(val1)),
+                Fq::fq(fq_field_modulus.clone(), BigInt::from(val2))
+            ],
+            fq2_non_residue.clone(),
+            fq2_forbenius_coeffs_c1.clone()
+        );
+    }
+
+    fn get_fq2_str(val1: &str, val2: &str) -> Fq2 {
+        let fq_field_modulus = BigInt::from_str("21888242871839275222246405745257275088696311157297823662689037894645226208583").unwrap();
+        let fq2_non_residue = Fq::fq(fq_field_modulus.clone(), BigInt::from_str("21888242871839275222246405745257275088696311157297823662689037894645226208582").unwrap());
+        let fq2_forbenius_coeffs_c1 = [Fq::fq(fq_field_modulus.clone(), BigInt::from(1)),Fq::fq(fq_field_modulus.clone(), BigInt::from_str("21888242871839275222246405745257275088696311157297823662689037894645226208582").unwrap())];
+        return Fq2::fq2(
+            [
+                Fq::fq(fq_field_modulus.clone(), BigInt::from_str(val1).unwrap()),
+                Fq::fq(fq_field_modulus.clone(), BigInt::from_str(val2).unwrap())
+            ],
+            fq2_non_residue.clone(),
+            fq2_forbenius_coeffs_c1.clone()
+        );
+    }
+
+    fn get_fq6_forbenius_coeff_c1() -> [Fq2; 6] {
+        return [
+            get_fq2(BigInt::from(1), BigInt::from(0)),
+            get_fq2(BigInt::from_str("21575463638280843010398324269430826099269044274347216827212613867836435027261").unwrap(),BigInt::from_str("10307601595873709700152284273816112264069230130616436755625194854815875713954").unwrap()),
+            get_fq2(BigInt::from_str("21888242871839275220042445260109153167277707414472061641714758635765020556616").unwrap(), BigInt::from(0)),
+            get_fq2(BigInt::from_str("3772000881919853776433695186713858239009073593817195771773381919316419345261").unwrap(), BigInt::from_str("2236595495967245188281701248203181795121068902605861227855261137820944008926").unwrap()),
+            get_fq2(BigInt::from_str("2203960485148121921418603742825762020974279258880205651966").unwrap(), BigInt::from(0)),
+            get_fq2(BigInt::from_str("18429021223477853657660792034369865839114504446431234726392080002137598044644").unwrap(), BigInt::from_str("9344045779998320333812420223237981029506012124075525679208581902008406485703").unwrap())
+        ]
+    }
+
+    fn get_fq6_forbenius_coeff_c2() -> [Fq2; 6] {
+        return [
+            get_fq2(BigInt::from(1), BigInt::from(0)),
+            get_fq2(BigInt::from_str("2581911344467009335267311115468803099551665605076196740867805258568234346338").unwrap(),BigInt::from_str("19937756971775647987995932169929341994314640652964949448313374472400716661030").unwrap()),
+            get_fq2(BigInt::from_str("2203960485148121921418603742825762020974279258880205651966").unwrap(), BigInt::from(0)),
+            get_fq2(BigInt::from_str("5324479202449903542726783395506214481928257762400643279780343368557297135718").unwrap(), BigInt::from_str("16208900380737693084919495127334387981393726419856888799917914180988844123039").unwrap()),
+            get_fq2(BigInt::from_str("21888242871839275220042445260109153167277707414472061641714758635765020556616").unwrap(), BigInt::from(0)),
+            get_fq2(BigInt::from_str("13981852324922362344252311234282257507216387789820983642040889267519694726527").unwrap(), BigInt::from_str("7629828391165209371577384193250820201684255241773809077146787135900891633097").unwrap())
+        ]
+    }
+
+    fn get_fq6(val: [&str; 6]) -> Fq6 {
+        let fq6_non_residue = get_fq2(BigInt::from(9), BigInt::from(1));
+        assert_eq!(val.len(), 6);
+
+        let a = get_fq2(BigInt::from_str(val[0].clone()).unwrap(), BigInt::from_str(val[1].clone()).unwrap());
+        let b = get_fq2(BigInt::from_str(val[2].clone()).unwrap(), BigInt::from_str(val[3].clone()).unwrap());
+        let c = get_fq2(BigInt::from_str(val[4].clone()).unwrap(), BigInt::from_str(val[5].clone()).unwrap());
+        return Fq6::fq6(
+            [a.clone(), b.clone(), c.clone()],
+            fq6_non_residue,
+            get_fq6_forbenius_coeff_c1(),
+            get_fq6_forbenius_coeff_c2()
+        )
+    }
+
+    fn get_fq12_forbenius_coeffs_c1() -> [Fq2; 12] {
+        return [
+            get_fq2_str("1", "0"),
+            get_fq2_str("8376118865763821496583973867626364092589906065868298776909617916018768340080", "16469823323077808223889137241176536799009286646108169935659301613961712198316"),
+            get_fq2_str("21888242871839275220042445260109153167277707414472061641714758635765020556617", "0"),
+            get_fq2_str("11697423496358154304825782922584725312912383441159505038794027105778954184319", "303847389135065887422783454877609941456349188919719272345083954437860409601"),
+            get_fq2_str("21888242871839275220042445260109153167277707414472061641714758635765020556616", "0"),
+            get_fq2_str("3321304630594332808241809054958361220322477375291206261884409189760185844239", "5722266937896532885780051958958348231143373700109372999374820235121374419868"),
+            get_fq2_str("21888242871839275222246405745257275088696311157297823662689037894645226208582", "0"),
+            get_fq2_str("13512124006075453725662431877630910996106405091429524885779419978626457868503", "5418419548761466998357268504080738289687024511189653727029736280683514010267"),
+            get_fq2_str("2203960485148121921418603742825762020974279258880205651966", "0"),
+            get_fq2_str("10190819375481120917420622822672549775783927716138318623895010788866272024264", "21584395482704209334823622290379665147239961968378104390343953940207365798982"),
+            get_fq2_str("2203960485148121921418603742825762020974279258880205651967", "0"),
+            get_fq2_str("18566938241244942414004596690298913868373833782006617400804628704885040364344", "16165975933942742336466353786298926857552937457188450663314217659523851788715"),
+        ]
+    }
+
+
+    #[test]
+    fn test_fq12_add() {
+        let fq12_non_residue = get_fq2(BigInt::from(9), BigInt::from(1));
+        let fq12_forbenius_coeffs_c1 = get_fq12_forbenius_coeffs_c1();
+        let a = get_fq6(["1", "2", "1", "2", "1", "2"]);
+        let b = get_fq6(["2", "2", "5", "2", "10", "2"]);
+        let a1 = get_fq6(["10", "2", "1", "23", "12", "24"]);
+        let b1 = get_fq6(["2", "21", "54", "21", "110", "52"]);
+        let c = Fq12::fq12([a, b], fq12_non_residue.clone(), fq12_forbenius_coeffs_c1.clone());
+        let c1 = Fq12::fq12([a1, b1], fq12_non_residue.clone(), fq12_forbenius_coeffs_c1.clone());
+        let res_a = get_fq6(["11", "4", "2", "25", "13", "26"]);
+        let res_b = get_fq6(["4", "23", "59", "23", "120", "54"]);
+        let res = Fq12::fq12([res_a, res_b], fq12_non_residue.clone(), fq12_forbenius_coeffs_c1.clone());
+        let d = c + c1;
+        assert_eq!(d, res);
+    }
+
+    #[test]
+    fn test_fq12_sub() {
+        let fq12_non_residue = get_fq2(BigInt::from(9), BigInt::from(1));
+        let fq12_forbenius_coeffs_c1 = get_fq12_forbenius_coeffs_c1();
+        let a = get_fq6(["1", "2", "1", "2", "1", "2"]);
+        let b = get_fq6(["2", "2", "5", "2", "10", "2"]);
+        let a1 = get_fq6(["10", "2", "1", "23", "12", "24"]);
+        let b1 = get_fq6(["2", "21", "54", "21", "110", "52"]);
+        let c = Fq12::fq12([a, b], fq12_non_residue.clone(), fq12_forbenius_coeffs_c1.clone());
+        let c1 = Fq12::fq12([a1, b1], fq12_non_residue.clone(), fq12_forbenius_coeffs_c1.clone());
+        let res_a = get_fq6(["9", "0", "0", "21", "11", "22"]);
+        let res_b = get_fq6(["0", "19", "49", "19", "100", "50"]);
+        let res = Fq12::fq12([res_a, res_b], fq12_non_residue.clone(), fq12_forbenius_coeffs_c1.clone());
+        let d = c1 - c;
+        assert_eq!(d, res);
+    }
+
+
+    #[test]
+    fn test_fq12_mul() {
+        let fq12_non_residue = get_fq2(BigInt::from(9), BigInt::from(1));
+        let fq12_forbenius_coeffs_c1 = get_fq12_forbenius_coeffs_c1();
+        let a = get_fq6(["1", "2", "1", "2", "1", "2"]);
+        let b = get_fq6(["2", "2", "5", "2", "10", "2"]);
+        let a1 = get_fq6(["10", "2", "1", "23", "12", "24"]);
+        let b1 = get_fq6(["2", "21", "54", "21", "110", "52"]);
+        let c = Fq12::fq12([a, b], fq12_non_residue.clone(), fq12_forbenius_coeffs_c1.clone());
+        let c1 = Fq12::fq12([a1, b1], fq12_non_residue.clone(), fq12_forbenius_coeffs_c1.clone());
+        let res_a = get_fq6(["1351", "7679", "7249", "8615", "8183", "8010"]);
+        let res_b = get_fq6(["21888242871839275222246405745257275088696311157297823662689037894645226207728", "7036", "140", "5134", "9", "655"]);
+        let res = Fq12::fq12([res_a, res_b], fq12_non_residue.clone(), fq12_forbenius_coeffs_c1.clone());
+        let d = c * c1;
+        assert_eq!(d, res);
+    }
+
+    #[test]
+    fn test_fq12_inverse() {
+        let fq12_non_residue = get_fq2(BigInt::from(9), BigInt::from(1));
+        let fq12_forbenius_coeffs_c1 = get_fq12_forbenius_coeffs_c1();
+        let a = get_fq6(["1", "2", "1", "2", "1", "2"]);
+        let b = get_fq6(["2", "2", "5", "2", "10", "2"]);
+        let c = Fq12::fq12([a, b], fq12_non_residue.clone(), fq12_forbenius_coeffs_c1.clone());
+        let c_inv = c.inverse();
+        let fq12_one = Fq12::fq12([get_fq6(["1", "0", "0", "0", "0", "0"]), get_fq6(["0", "0", "0","0", "0", "0"])],
+                                  fq12_non_residue.clone(),
+                                  fq12_forbenius_coeffs_c1.clone());
+        assert_eq!(c * c_inv, fq12_one);
+    }
+
+    #[test]
+    fn test_fq12_div() {
+        let fq12_non_residue = get_fq2(BigInt::from(9), BigInt::from(1));
+        let fq12_forbenius_coeffs_c1 = get_fq12_forbenius_coeffs_c1();
+        let a = get_fq6(["1", "2", "1", "2", "1", "2"]);
+        let b = get_fq6(["2", "2", "5", "2", "10", "2"]);
+        let a1 = get_fq6(["10", "2", "1", "23", "12", "24"]);
+        let b1 = get_fq6(["2", "21", "54", "21", "110", "52"]);
+        let c = Fq12::fq12([a, b], fq12_non_residue.clone(), fq12_forbenius_coeffs_c1.clone());
+        let c1 = Fq12::fq12([a1, b1], fq12_non_residue.clone(), fq12_forbenius_coeffs_c1.clone());
+        let d = c.clone() * c1.clone();
+        let e = d.clone() / c1.clone();
+        assert_eq!(e, c);
+    }
+
+    #[test]
+    fn test_fq12_forbenius_map() {
+        todo!()
     }
 }
 
